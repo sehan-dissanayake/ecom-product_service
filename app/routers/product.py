1a4072f5db49aec734a6c2bf22cdec431b706eb4
sehan <sehan.22@cse.mrt.ac.lk>
2025-04-17 19:39:50 +0530

    Update product.py

--- app/routers/product.py
+++ app/routers/product.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, HTTPException
 from app.models.product import Product
-from app.schemas.product import ProductCreate, Product as ProductSchema
+from app.schemas.product import ProductCreate
 
 router = APIRouter(prefix="/products", tags=["products"])
 
-@router.post("/", response_model=ProductSchema)
+@router.post("/", response_model=Product)
 async def create_product(product: ProductCreate):
     new_product = Product(**product.model_dump())
     await new_product.insert()
