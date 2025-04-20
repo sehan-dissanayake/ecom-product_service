924c84a904f760b739b4ee91792b76ac647cec25
sehan <sehan.22@cse.mrt.ac.lk>
2025-04-20 13:37:34 +0530

    Update auth.py

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -2,7 +2,7 @@ from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose import JWTError, jwt
 
-SECRET_KEY = "your-secret-key-here"
+SECRET_KEY = "14825069c25f0aa10c1d59aac7788de1b571f10e0421dc0a3b08cbd632f6b1e0"
 ALGORITHM = "HS256"
 
 security = HTTPBearer()
